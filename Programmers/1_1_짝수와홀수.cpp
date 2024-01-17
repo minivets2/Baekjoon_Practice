e3edf4e15243636c1c90c37a40ac0c498896fed9
BOOK-OKPNPUFST0\onejo <onejourney27@gmail.com>
2024-01-17 14:55:06 +0900

    프로그래머스 문제 업로드

---
+++ Programmers/1_1_짝수와홀수.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num)
+{
+    string answer = "Odd";
+
+    if (num % 2 == 0)
+        answer = "Even";
+
+    return answer;
+}
